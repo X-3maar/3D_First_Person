5a2bd5dc89c5639dabb5d60cd7ae459200681256
3maar1 <amarmostafaabdelhady265@gmail.com>
2026-06-25 01:16:38 +0300

    finally fixed animations

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -11,60 +11,45 @@ const BULLET = preload("uid://dl5uh1glkohgb")
 var aiming = false
 var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var moving
 var captured = true
 var running = true
 var paused = false
 @onready var neck: Node3D = $Neck
 @onready var camera_3d: Camera3D = $Neck/Camera3D
+
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			neck.rotate_y(-event.relative.x * 0.01)
 			camera_3d.rotate_x(-event.relative.y * 0.01)
 			camera_3d.rotation.x = clamp(camera_3d.rotation.x,deg_to_rad(-60),deg_to_rad(60))
+
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_front", "move_back")
 	var direction := (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if Input.is_action_pressed("move_front") or Input.is_action_pressed("move_back") or Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right") and direction and !aiming:
+	if direction.length() > 0:
+		moving = true
+	else:
+		moving = false
+	if moving and !aiming and !pistol.is_playing():
 		camera.play("Walk")
-		pistol.play("Walk")
-		if Input.is_action_just_pressed("shoot"):
-			camera.stop()
-			pistol.stop()
-			pistol.play("Shooting")
-		else:
-			camera.play("Walk")
-			pistol.play("Walk")
-		if Input.is_action_pressed("run"):
-			pistol.speed_scale = 2.0
-			camera.speed_scale = 2.0
-			SPEED = 10
-			running = true
-		elif Input.is_action_just_released("run") and Input.is_action_pressed("move_front"):
-			pistol.speed_scale = 1.0
-			camera.speed_scale = 1.0
-			SPEED = 5
-			running = false
-
-	elif Input.is_action_just_pressed("aim") and !aiming:
-		pistol.stop()
+	elif !moving and !aiming:
+		camera.stop()
+	if Input.is_action_just_pressed("aim") and !aiming:
 		camera.stop()
 		pistol.play("aimin")
 		camera.play("aimin")
 		aiming = true
-		if Input.is_action_just_pressed("move_front") or Input.is_action_just_pressed("move_back") or Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right"):
-			SPEED = 2
-		else:
-			SPEED = 5
+		
 	elif Input.is_action_just_pressed("aim") and aiming:
 		pistol.play("aimout")
 		camera.play("aimout")
-		await pistol.animation_finished
-		await camera.animation_finished
 		aiming = false
-	
+		
 	if Input.is_action_just_pressed("esc") and !paused:
 		color_rect.show()
 		color_rect_2.show()
@@ -94,45 +79,35 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	if direction and Input.is_action_pressed("run"):
 		running = true
 	else:
 		running = false		
-	if direction :
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		
 	if running:
+		camera.speed_scale = 2.0
 		SPEED = 10
 	else:
+		camera.speed_scale = 1.0
 		SPEED = 5
 			
-	if Input.is_action_just_released("move_back") or Input.is_action_just_released("move_front") or Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right") and !direction:
-		pistol.stop()
-		camera.stop()
-		
-
-	if Input.is_action_just_released("move_back") or Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right") or Input.is_action_just_released("move_front"):
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
 	
-	if !direction:
-		velocity.x = 0
-		velocity.z = 0
 	move_and_slide()
-
 
 func _on_exit_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
-
 
 func _on_conitnue_pressed() -> void:
 	color_rect.hide()
@@ -151,6 +126,7 @@ func _on_button_pressed() -> void:
 	cross_hair.hide()
 	paused = true
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
 func shoot():
 	var bullet = BULLET.instantiate()
 	shooter.add_child(bullet)
