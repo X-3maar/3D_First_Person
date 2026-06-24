b754cf157b7c610a2e9785f9375adc4b9a3e957e
3maar1 <amarmostafaabdelhady265@gmail.com>
2026-06-24 17:28:42 +0300

    created the run ability

--- Scripts/Sea.gd
+++ Scripts/Sea.gd
@@ -3,4 +3,4 @@ extends Area3D
 
 
 func _on_body_entered(body: Node3D) -> void:
-	get_tree().reload_current_scene()
+	get_tree().call_deferred("reload_current_scene")
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-const SPEED = 5.0
+var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var captured = true
+var running = true
 @onready var neck: Node3D = $Neck
 @onready var camera_3d: Camera3D = $Neck/Camera3D
 func _unhandled_input(event: InputEvent) -> void:
@@ -32,12 +34,25 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_front", "move_back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction and Input.is_action_pressed("run"):
+		running = true
+	else:
+		running = false
+	if running:
+		SPEED = 10
+		animation_player.speed_scale = 1.5
+	else:
+		SPEED = 5
+		animation_player.speed_scale = 1.0
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-	else:
+		animation_player.play("walk")
+
+	elif Input.is_action_just_released("move_back") or Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right") or Input.is_action_just_released("move_front"):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		animation_player.stop()
+		
 	move_and_slide()
