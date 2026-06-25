bc5e2739e074ba6c046298b39aa8150ac0665041
3maar1 <amarmostafaabdelhady265@gmail.com>
2026-06-25 15:47:15 +0300

    struggling with the multiplayer

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,27 +1,54 @@
 extends CanvasLayer
 
+@onready var online: Button = $Online
+@onready var button_2: Button = $Button2
+@onready var label: Label = $Label
+@onready var color_rect_2: ColorRect = $ColorRect2
+@onready var ip: LineEdit = $ColorRect2/LineEdit
 
-# Called when the node enters the scene tree for the first time.
+const PORT = 6760
+var enet_peer = ENetMultiplayerPeer.new()
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	multiplayer.connected_to_server.connect(_on_connection_success)
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/online.tscn")
 
-
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu1.tscn")
-
 
 func _on_button_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
 
-
 func _on_button_4_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/offline.tscn")
+
+func _on_online_pressed() -> void:
+	button_2.hide()
+	label.hide()
+	online.hide()
+	color_rect_2.show()
+
+func _on_host_3_pressed() -> void:
+	button_2.show()
+	label.show()
+	online.show()
+	color_rect_2.hide()
+
+func _on_host_pressed() -> void:
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	load_game_scene()
+
+func _on_join_pressed() -> void:
+
+	enet_peer.create_client("local_host", PORT)
+
+	multiplayer.multiplayer_peer = enet_peer
+
+func _on_connection_success() -> void:
+	load_game_scene()
+
+func load_game_scene() -> void:
+	get_tree().change_scene_to_file("res://Scenes/online.tscn")
--- Scripts/target.gd
+++ Scripts/target.gd
@@ -1,6 +1,8 @@
 extends Area3D
 var count = 0
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
 # Called when the node enters the scene tree for the first time.
+@onready var animation_player: AnimationPlayer = $"../Gate/AnimationPlayer"
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -8,7 +10,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if count == 3:
-		queue_free()
+		animation_player.play("open")
+		process_mode = PROCESS_MODE_DISABLED
 
 
 
